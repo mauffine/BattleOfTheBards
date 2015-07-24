ede7633f1c752585bbdc102052a72dc81a960a32
Sean Mauff <sjmauff@hotmail.com>
2015-07-24 15:31:04 +1000

    -Finished the Turn Timer Class -Added Battle.cs and battle handler code     -set up the battle state for a win/lose state     -added player turn     -added a callback function for input Manager to use, which passes the key that it gets passed to sound manager, bard and spell system for those classes to use -Added Bunny.fbx for stuff, realistically I have  no idea....

---
+++ BotB/Assets/_Scripts/Battle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Battle : MonoBehaviour 
+{
+    //Attributes
+    Dictionary<KeyCode, string> m_notes;
+    
+    bool m_win;
+    bool m_playing;
+
+    [SerializeField]
+    bool m_playerTurn;
+    //Behavious
+	void Start () 
+    {
+        m_playerTurn = true;
+	}
+	
+	void Update () 
+    {
+	
+	}
+    //Receives valid key presses from InputManager and passes them out to all the relevant scripts if
+    //it's a not or quits and loads the menu if it's the escape button
+    public void ReceiveKey(KeyCode a_key)
+    {
+        if (m_notes.ContainsKey(a_key))
+        {
+            string note;
+            m_notes.TryGetValue(a_key, out note); //grab the note out of a dictionary
+            SendMessage("PlayNote", note); //send it to everyone with a "PlayNote" method
+        }
+        else if (a_key == KeyCode.Escape)
+        {
+            //go back to menu
+            //Application.LoadLevel("Menu")
+        }
+    }
+    public void RecieveTurnOver(bool a_playerTurn)
+    {
+        m_playerTurn = a_playerTurn;
+        //change turn and notify SpellSystem to cast spells
+    }
+}
--- BotB/Assets/_Scripts/TurnTimer.cs
+++ BotB/Assets/_Scripts/TurnTimer.cs
@@ -1,31 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class TurnTimer : MonoBehaviour {
     //Attributes
     public float m_turnTime; //the time in seconds for each turn
     public bool m_playerTurn; //bool for it it's player's turn or enemy's turn
 
+    [SerializeField]
     float m_turnCountdown; //the countdown variable used in the timer 
     
     //Behavious
 	void Start () {
-        m_turnTime = 8;
-        m_playerTurn = true;
-        m_turnCountdown = m_turnTime;
+        m_turnTime = 8; //8 seconds per turn   
+        m_playerTurn = true; //start on the player turn
+        m_turnCountdown = m_turnTime; //set the countdown to the turn time
 	}
 	
 	void Update () {
         TurnCountdown();
 	}
 
-    void TurnCountdown()
+    void TurnCountdown() //Function to count down the time left in the turn, switches turns when the timer ends and restarts it
     {
+        //basic timer stuff
         m_turnCountdown -= Time.deltaTime;
         if (m_turnCountdown <= 0)
         {
             m_turnCountdown = m_turnTime;
             m_playerTurn = !m_playerTurn;
+            Battle battleScript = gameObject.GetComponent<Battle>();
+            battleScript.RecieveTurnOver(m_playerTurn);
         }
     }
 }
