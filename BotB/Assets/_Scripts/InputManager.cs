a4a344870dded7af81206f6351e30341dac4da0a
Daniel Waterston <DanielAfroHead@live.com.au>
2015-08-11 15:00:30 +1000

    Should be the final merge

--- BotB/Assets/_Scripts/InputManager.cs
+++ BotB/Assets/_Scripts/InputManager.cs
@@ -13,6 +13,8 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] ///<summary>The current input method being used</summary>
     private ControllerType m_playerInput = ControllerType.keyboard;
+    [SerializeField]
+    private KeyCode aNote = KeyCode.Z, bNote = KeyCode.X, cNote = KeyCode.C, dNote = KeyCode.V, eNote = KeyCode.Space;
 	void Start () 
     {
 	
@@ -25,15 +27,15 @@ public class InputManager : MonoBehaviour
            float time = this.GetComponent<TurnTimer>().CountdownTime;
            //A whole bunch of key checks
            //I have tried to lay the notes out to correspond with the key presses musically
-           if (Input.GetKeyDown(KeyCode.W))
+           if (Input.GetKeyDown(aNote))
                Battle.ReceiveKey(new TimedNote(Note.A, time));
-           if (Input.GetKeyDown(KeyCode.S))
-               Battle.ReceiveKey(new TimedNote(Note.D, time));
-           if (Input.GetKeyDown(KeyCode.A))
+           if (Input.GetKeyDown(bNote))
                Battle.ReceiveKey(new TimedNote(Note.B, time));
-           if (Input.GetKeyDown(KeyCode.D))
+           if (Input.GetKeyDown(cNote))
                Battle.ReceiveKey(new TimedNote(Note.C, time));
-           if (Input.GetKeyDown(KeyCode.Space))
+           if (Input.GetKeyDown(dNote))
+               Battle.ReceiveKey(new TimedNote(Note.D, time));
+           if (Input.GetKeyDown(eNote))
                Battle.ReceiveKey(new TimedNote(Note.E, time)); 
        }
       
--- BotB/Assets/_Scripts/Spell.cs
+++ BotB/Assets/_Scripts/Spell.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class Spell : MonoBehaviour 
 {
     float m_lifetime;//How long the spell particles lasts= for
-    Vector3 m_transform;//The transform
+    Vector3 m_velocity;//The velocity
     string m_key;//A key to show what key
 	// Use this for initialization
 	void Start() 
     {
         m_lifetime = 2.0f;
-        m_transform = new Vector3(0.03f, Random.Range(-0.01f, 0.007f), 0);
+        m_velocity = new Vector3(-.03f, Random.Range(-.007f, 00f), 0);
 	}
 	// Update is called once per frame
 	void Update() 
     {
-        this.transform.Translate(m_transform);
+        this.transform.Translate(m_velocity);
         m_lifetime -= Time.deltaTime;
 
         if (m_lifetime <= 0)
