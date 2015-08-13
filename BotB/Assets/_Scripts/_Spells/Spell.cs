7309881903cdfa7fb5b08fe64ee3f333b5146a31
Sean Mauff <sjmauff@hotmail.com>
2015-08-13 16:36:48 +1000

    May as well commit this, fixed spell velocity to use deltaTime, it was shooting off the screen on high frame rates

--- BotB/Assets/_Scripts/_Spells/Spell.cs
+++ BotB/Assets/_Scripts/_Spells/Spell.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class Spell : MonoBehaviour 
 {
+    public float m_speedModifier;//modifier for deltaTime
     public Vector3 m_velocity;//The velocity
     protected uint m_damage; //Damage that the spell deals
     protected string m_key;//A key to show what key
 	// Use this for initialization
 	void Start() 
     {
+        m_speedModifier = 50;
         m_damage = 20;
 	}
 	// Update is called once per frame
 	void Update() 
     {
-        this.transform.Translate(m_velocity);
+        transform.Translate(m_velocity * Time.deltaTime * m_speedModifier);
 	}
     public uint Damage
     {
