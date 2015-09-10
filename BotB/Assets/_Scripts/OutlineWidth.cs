ae497b9ee092e2d48235780ed6156475faff97d4
Richard Murray <arraygear2@gmail.com>
2015-09-10 16:36:48 +1000

    Implemented new outlining system, helper script allows changing separate instance outlines.

---
+++ BotB/Assets/_Scripts/OutlineWidth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineWidth : MonoBehaviour {
+
+    public float m_outlineWidth;
+    private Material m_material;
+
+	// Use this for initialization
+	void Start () {
+        var meshRenderer = transform.GetComponent<SkinnedMeshRenderer>();
+        if (meshRenderer.materials.Length > 1)
+        {
+            m_material = meshRenderer.materials[1];
+            m_material.SetFloat("_Outline", m_outlineWidth);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+}
