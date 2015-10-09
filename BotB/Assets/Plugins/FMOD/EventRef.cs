07d4315413a19bff39157f25ff4ff6823d35148e
Brock Wills <b.m.wills7@gmail.com>
2015-10-09 16:00:09 +1100

    Added FMOD for Unity.

---
+++ BotB/Assets/Plugins/FMOD/EventRef.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace FMODUnity
+{
+    [Serializable]
+    public class EventRef
+    {
+        public string Path;
+
+        // public void PlayOneShot();
+        // public void PlayOneShot(GameObject parentObject);
+        // public void PlayOneShot(GameObject parentObject, Vector3 offset);
+        // public void PlayOneShot(Vector3 position);
+        // public void PlayOneShot(Dictionary<string, float> parameters);
+        // 
+        // public void StartInstance();
+        // public void StopInstance();
+        // 
+        // public void SetPosition(GameObject parentObject);
+        // public void SetPosition(GameObject parentObject, Vector3 offset);
+        // public void SetPosition(Vector3 position);
+        // public void SetParameter(string name, float value);
+    }
+}
---
+++ BotB/Assets/Plugins/FMOD/StudioBankLoader.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace FMODUnity
+{
+    [AddComponentMenu("FMOD Studio/FMOD Studio Bank Loader")]
+    public class StudioBankLoader : MonoBehaviour
+    {
+
+        public BeginEvent LoadEvent;
+        public EndEvent UnloadEvent;
+        public BankRefList Banks;
+        public String CollisionTag;
+        public bool PreloadSamples;
+
+        Collider triggerObject;
+        bool loaded;
+      
+        void OnEnable()
+        {
+            if (LoadEvent == BeginEvent.LevelStart)
+            {
+                Load();
+                loaded = true;
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            loaded = false;
+        }
+
+        void OnDisable()
+        {
+            if (UnloadEvent == EndEvent.LevelEnd && loaded)
+            {
+                Unload();
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (LoadEvent == BeginEvent.TriggerEnter && 
+                triggerObject == null &&
+                (String.IsNullOrEmpty(CollisionTag) || other.tag == CollisionTag))
+            {
+                triggerObject = other;
+                Load();
+                loaded = true;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (UnloadEvent == EndEvent.TriggerExit && other == triggerObject && loaded)
+            {
+                Unload();
+                triggerObject = null;
+            }
+        }
+
+        public void Load()
+        {
+            foreach (var bankRef in Banks)
+            {
+                try
+                {
+                    RuntimeManager.LoadBank(bankRef, PreloadSamples);
+                }
+                catch (BankLoadException e)
+                {
+                    UnityEngine.Debug.LogException(e);
+                }
+            }
+        }
+
+        public void Unload()
+        {
+            foreach (var bankRef in Banks)
+            {
+                RuntimeManager.UnloadBank(bankRef);
+            }
+        }
+    }
+}
