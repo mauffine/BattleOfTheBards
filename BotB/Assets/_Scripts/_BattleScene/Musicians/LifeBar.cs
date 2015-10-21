576db80ee7a833fdcfcaa985f7ccabde7cb67f5f
Daniel Waterston <DanielAfroHead@live.com.au>
2015-10-21 12:27:46 +1100

    Fixed life bar

--- BotB/Assets/_Scripts/_BattleScene/Musicians/LifeBar.cs
+++ BotB/Assets/_Scripts/_BattleScene/Musicians/LifeBar.cs
@@ -8,18 +8,20 @@ public class LifeBar : MonoBehaviour
     public SpriteRenderer m_rend;
     [SerializeField]
     Musician m_me = null;
-    float m_barLen = -1.0f, m_barFull = -1.0f;
+    float m_barLen = -1.0f;
+    float m_barFull = 0.425f;//Magic number for scale
     int m_health;
+    int m_fullHealth;
 	// Use this for initialization
-	void Start() 
+	void Start()
     {
         m_rend.color = Color.green;
         m_sprite = Instantiate(m_sprite);
         m_rend.sprite = m_sprite;
-        m_sprite.border.Set(0,0,1000,50);
         m_health = m_me.Health;
-        m_barFull = m_barLen = m_sprite.border.z;
-        m_me.transform.parent = transform;
+        m_barLen = m_barFull;
+        m_fullHealth = m_health;
+        m_barFull = m_rend.transform.localScale.x;
 	}
 	
 	// Update is called once per frame
@@ -27,7 +29,10 @@ public class LifeBar : MonoBehaviour
     {
         m_barLen = m_barFull;
         m_health = m_me.Health;
-        m_barLen -= (m_sprite.border.z / m_health);
-        m_sprite.border.Set(m_sprite.border.x, m_sprite.border.y, m_barLen, m_sprite.border.w);
+        m_barLen *= ((float)m_health / m_fullHealth);
+        if (float.IsInfinity(m_barLen))
+            m_barLen = 0.00000000000000001f;
+        else
+            m_rend.GetComponent<Transform>().localScale = new Vector3(m_barLen, 1, 0);
 	}
 }
